48dfd0a62fe4a019e6b00f86c6d36a3117a9b496
tomassueldo <tomassueldo17@gmail.com>
2021-11-25 17:52:08 -0300

    cambios

--- index.php
+++ index.php
@@ -18,15 +18,14 @@ $pg = "inicio";
     <link rel="stylesheet" href="css/fontawesome/css/fontawesome.min.css">
     <link rel="stylesheet" href="css/estilos.css">
     <link rel="shortcut icon" href="images/cv.ico">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap" rel="stylesheet">
 </head>
 
 
 <body id="inicio">
 
 
-    <?php include_once("header.php");?>
+    <?php include_once("header.php"); ?>
 
 
     <main class="container">
@@ -36,11 +35,8 @@ $pg = "inicio";
             </div>
             <div class="row">
                 <div class="col-12 col-sm-6 mt-5 offset-sm-3 text-center">
-                    <p>Hola!
-                        <br>
-                        Bienvenido a mi sitio web
-                        <br>
-                        Tomas Sueldo
+                    <p id="pTexto" class="pTexto">
+                       ¡Bienvenido a mi sitio web!
                     </p>
                 </div>
                 <div class="col-12 text-center">
@@ -51,10 +47,34 @@ $pg = "inicio";
     </main>
 
 
-    <?php include_once("footer.php");?>
+    <?php include_once("footer.php"); ?>
 
 
     <script src="css/bootstrap/js/bootstrap.bundle.min.js   "></script>
+
+
+    <script>
+     
+        var txt = document.getElementById("pTexto");
+        var str = txt.innerHTML;
+
+        txt.innerHTML = "";
+
+        var speed = 50;
+        var i = 0;
+        
+        
+        function typeWriter() {
+            if(i < str.length) {
+                txt.innerHTML += str.charAt(i);
+                i++;
+                setTimeout(typeWriter, speed);
+            }
+        }
+
+        setTimeout(typeWriter, speed);
+
+    </script>
 
 
 </body>
